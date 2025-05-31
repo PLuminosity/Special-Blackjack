3ee05ccf66bad09a206fbab720de96cdaab20d90
Andreas Pham <PLumiDev@gmail.com>
2025-05-31 21:27:54 +0200

    Fixed Reset hand wild card - it no longer deletes the players entire hand

--- src/ResetHandCardI.java
+++ src/ResetHandCardI.java
@@ -7,6 +7,10 @@ public class ResetHandCardI implements IWildCard {
 
     @Override
     public void activate(Player player, Dealer dealer) {
+        Card firstCard = player.getHand().getCards().getFirst();
+        Card secondCard = player.getHand().getCards().get(1);
         player.resetHand();
+        player.getHand().addCard(firstCard);
+        player.getHand().addCard(secondCard);
     }
 }
